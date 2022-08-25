dd3e218b07526a24147881615680ab44744d0d56
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2022-08-25 12:46:25 +0500

    build

---
+++ precache-manifest.3bbbdb063c89c801af270c7e0f2d57dd.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299bcbf38a40b34c1e308bd549830d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc2ca57a72336624f64",
+    "url": "/static/css/2.72696988.chunk.css"
+  },
+  {
+    "revision": "de021e0603f92999cbc8",
+    "url": "/static/css/main.5f48d2b9.chunk.css"
+  },
+  {
+    "revision": "4bc2ca57a72336624f64",
+    "url": "/static/js/2.ea0205f3.chunk.js"
+  },
+  {
+    "revision": "6d112228ddfeb93266a5b1848e39459f",
+    "url": "/static/js/2.ea0205f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de021e0603f92999cbc8",
+    "url": "/static/js/main.84e3e5b4.chunk.js"
+  },
+  {
+    "revision": "2869dda80d51613d8ad9",
+    "url": "/static/js/runtime-main.6fa4b921.js"
+  },
+  {
+    "revision": "a59a10fc173a405262ecba082980066e",
+    "url": "/static/media/AktivGrotesk.a59a10fc.ttf"
+  },
+  {
+    "revision": "65fd215595eff431feb7b0090b31f78a",
+    "url": "/static/media/Lessons.65fd2155.png"
+  },
+  {
+    "revision": "403d8f250ee7806bb4f053dac48ef173",
+    "url": "/static/media/PrivacyPolicy.403d8f25.docx"
+  },
+  {
+    "revision": "70e032373fa11efe71c51317b77bd880",
+    "url": "/static/media/a.70e03237.png"
+  },
+  {
+    "revision": "3ec9c960d85fffae8e85d3adffc98cf7",
+    "url": "/static/media/b.3ec9c960.png"
+  },
+  {
+    "revision": "47d72e437623c26a97eba87a9cdd8ee4",
+    "url": "/static/media/baloon.47d72e43.png"
+  },
+  {
+    "revision": "13c328457d1f2be64d73f371b0abb622",
+    "url": "/static/media/c.13c32845.png"
+  },
+  {
+    "revision": "1e94beb4b3c8c30e3ebecffc05f5b302",
+    "url": "/static/media/calenders.1e94beb4.png"
+  },
+  {
+    "revision": "f3bafc9685465337f20ca3e203e13b9f",
+    "url": "/static/media/commentPic.f3bafc96.png"
+  },
+  {
+    "revision": "8e253cb698c86a36524ffa6791c117a0",
+    "url": "/static/media/d.8e253cb6.png"
+  },
+  {
+    "revision": "c68e1c2faea7c511f7f5093e1997520f",
+    "url": "/static/media/logo.c68e1c2f.svg"
+  },
+  {
+    "revision": "e525c4044d9ef648091fd43526418662",
+    "url": "/static/media/medal.e525c404.png"
+  },
+  {
+    "revision": "50a47343a828fe4c66df44c2969e4db8",
+    "url": "/static/media/mobile.50a47343.png"
+  },
+  {
+    "revision": "44049d96adf003a2038dee8a882ca548",
+    "url": "/static/media/perfectTutor.44049d96.png"
+  },
+  {
+    "revision": "a49da6aac13721e49a4acbc544cfdbcd",
+    "url": "/static/media/question.a49da6aa.png"
+  },
+  {
+    "revision": "4003cc4a3e898852e774fd10369cf651",
+    "url": "/static/media/s1.4003cc4a.png"
+  },
+  {
+    "revision": "1f4e1a44588386f9bf1610495b972e43",
+    "url": "/static/media/s2.1f4e1a44.png"
+  },
+  {
+    "revision": "73671a83520cb0527e76d6b215d8b52e",
+    "url": "/static/media/s3.73671a83.png"
+  },
+  {
+    "revision": "07b335d74b45b26a5c3674a434e7c71d",
+    "url": "/static/media/teacher.07b335d7.jpeg"
+  },
+  {
+    "revision": "ead0e24aa028b121e427da32b8754ee9",
+    "url": "/static/media/teacher.ead0e24a.png"
+  },
+  {
+    "revision": "eeab3de1aa6015917ebe448149c6dcd2",
+    "url": "/static/media/tutorGirl.eeab3de1.png"
+  },
+  {
+    "revision": "93fb809d966003ea5e7b84ef65576cdc",
+    "url": "/static/media/w1.93fb809d.png"
+  },
+  {
+    "revision": "a61117ad498eb0238526cd41c453c5cb",
+    "url": "/static/media/wireGlobe.a61117ad.png"
+  }
+]);
